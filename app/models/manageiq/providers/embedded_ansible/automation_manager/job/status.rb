4dc5ba35df01ed398f3a473826561e127433ef1a
Nick LaMuro <nicklamuro@gmail.com>
2019-06-06 18:13:28 -0500

    [FIXUP] EmbeddedAnsible job/status_spec.rb
    Pulls in specs from the manageiq-providers-ansible_tower repo, but makes
    them relevant for the new ansible_runner code.
    
    There was some changes necessary in the models to make things trued up
    with what was done in the provider repo, which is a bit of a departure
    from what was originally done (though not much).  Mostly trying to keep
    things as close to how they worked previously, though as mentioned in
    the comments, OrchestrationStack::Status and MiqTask don't have a 1-to-1
    status comparison, so some "fudging" was necessary to keep things as DRY
    as possible.

--- app/models/manageiq/providers/embedded_ansible/automation_manager/job/status.rb
+++ app/models/manageiq/providers/embedded_ansible/automation_manager/job/status.rb
@@ -1,11 +1,24 @@
 class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::Job::Status < ::OrchestrationStack::Status
-  alias miq_task status
+  attr_accessor :task_status
+
+  # This is a bit confusing, but because the OrchestrationStack::Status doesn't
+  # have a concept of both state and status as MiqTask does, inverting is the
+  # "DRYest" way to match up with `normalized_status` found in
+  # OrchestrationStack::Status.
+  def initialize(miq_task, reason)
+    super(miq_task.state, reason)
+    self.task_status = miq_task.status
+  end
+
+  def completed?
+    status == MiqTask::STATE_FINISHED
+  end
 
   def succeeded?
-    miq_task.state == MiqTask::STATE_FINISHED && miq_task.status == MiqTask::STATUS_OK
+    completed? && task_status == MiqTask::STATUS_OK
   end
 
   def failed?
-    miq_task.state == MiqTask::STATE_FINISHED && miq_task.status != MiqTask::STATUS_OK
+    completed? && task_status != MiqTask::STATUS_OK
   end
 end
