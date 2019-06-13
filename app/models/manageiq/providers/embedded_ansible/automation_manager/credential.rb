7eb4b8377064920d531dcb10de063025334e997d
Nick LaMuro <nicklamuro@gmail.com>
2019-06-13 12:45:50 -0500

    [EmbeddedAnsible::Credential] Implement base CRUD
    Currently, this does nothing but inherits from crud common and sets up a
    base method for each inherited credential to build off of.  So for now,
    each class will need to implement `.params_to_attributes` for their
    specific use to work properly (to be done in follow up commits).
    
    Also removed notifications from credentials since they really don't make
    sense since there is no reason to push them into a background job.

--- app/models/manageiq/providers/embedded_ansible/automation_manager/credential.rb
+++ app/models/manageiq/providers/embedded_ansible/automation_manager/credential.rb
@@ -12,12 +12,45 @@ class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::Credential < Mana
 
   FRIENDLY_NAME = "Ansible Automation Inside Credential".freeze
 
+  include ManageIQ::Providers::EmbeddedAnsible::CrudCommon
+
   def self.provider_params(params)
     super.merge(:organization => ManageIQ::Providers::EmbeddedAnsible::AutomationManager.first.provider.default_organization)
   end
 
-  def self.notify_on_provider_interaction?
-    true
+  def self.params_to_attributes(_params)
+    raise NotImplementedError, "must be implemented in a subclass"
+  end
+
+  def self.raw_create_in_provider(_manager, params)
+    create!(params_to_attributes(params))
+  end
+
+  # Since credentials don't require any external resources, and should just be
+  # creating a single database record, stub the queue work requested by the UI,
+  # and just call the `raw` methods directly.
+  #
+  # TODO:  Update UI to not require the queue for credential creation
+  #
+  def self.create_in_provider_queue(manager_id, params, _auth_user = nil)
+    parent.find(manager_id) # validate manager
+    raw_create_in_provider(nil, params)
+  end
+
+  def update_in_provider_queue(params, _auth_user = nil)
+    raw_update_in_provider(params)
+  end
+
+  def delete_in_provider_queue(_auth_user = nil)
+    raw_delete_in_provider
+  end
+
+  def raw_update_in_provider(params)
+    update!(self.class.params_to_attributes(params.except(:task_id, :miq_task_id)))
+  end
+
+  def raw_delete_in_provider
+    destroy!
   end
 
   def native_ref
