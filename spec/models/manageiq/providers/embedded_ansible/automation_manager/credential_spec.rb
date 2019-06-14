15925a0d278ef99586ea238790305a80070fc613
Nick LaMuro <nicklamuro@gmail.com>
2019-06-13 19:13:08 -0500

    [EmbeddedAnsible::Credential] Add share example
    This shared example is in charge of most of the logic testing for each
    of the credential types under the EmbeddedAnsible umbrella.  The caller
    of this example group will just have to set the following `let` calls:
    
    - :credential_class     (the credential class constant)
    - :params               (sample create parameters from UI)
    - :params_to_attributes (sample create hash sent to ActiveRecord.create)
    - :expected_values      (expected values for the created record)

--- spec/models/manageiq/providers/embedded_ansible/automation_manager/credential_spec.rb
+++ spec/models/manageiq/providers/embedded_ansible/automation_manager/credential_spec.rb
@@ -1,4 +1,5 @@
 describe ManageIQ::Providers::EmbeddedAnsible::AutomationManager::Credential do
+  let(:embedded_ansible) { ManageIQ::Providers::EmbeddedAnsible::AutomationManager }
   let(:manager) do
     FactoryBot.create(:provider_embedded_ansible, :default_organization => 1).managers.first
   end
@@ -22,5 +23,82 @@ describe ManageIQ::Providers::EmbeddedAnsible::AutomationManager::Credential do
     end
   end
 
-  # it_behaves_like 'ansible credential'
+  shared_examples_for "an embedded_ansible credential" do
+    let(:base_excludes) { [:password, :auth_key, :service_account] }
+
+    context "CREATE" do
+      it ".create_in_provider creates a record" do
+        expect(credential_class).to receive(:create!).with(params_to_attributes).and_call_original
+        expect(Notification).to     receive(:create!).never
+
+        record = credential_class.create_in_provider(manager.id, params)
+
+        expect(record).to be_a(credential_class)
+        expected_values.each do |attr, val|
+          expect(record.send(attr)).to eq(val)
+        end
+      end
+
+      it ".create_in_provider_queue just calls raw_create_in_provider" do
+        expect(credential_class).to receive(:raw_create_in_provider).with(nil, params).and_call_original
+
+        record = credential_class.create_in_provider_queue(manager.id, params)
+
+        expect(record).to be_a(credential_class)
+        expected_values.each do |attr, val|
+          expect(record.send(attr)).to eq(val)
+        end
+      end
+
+      it ".create_in_provider_queue will fail with incompatible manager" do
+        wrong_manager = FactoryBot.create(:configuration_manager_foreman)
+        expect { credential_class.create_in_provider_queue(wrong_manager.id, params) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context "UPDATE" do
+      let(:update_params) { {:name => "Updated Credential" } }
+      let(:ansible_cred)  { credential_class.raw_create_in_provider(nil, params.merge(:manager => manager)) }
+
+      it "#update_in_provider to succeed" do
+        expect(Notification).to receive(:create!).never
+
+        result = ansible_cred.update_in_provider(update_params)
+
+        expect(result).to be_a(credential_class)
+        expect(result.name).to eq("Updated Credential")
+      end
+
+      it "#update_in_provider_queue just calls raw_update_in_provider" do
+        expect(ansible_cred).to receive(:raw_update_in_provider).with(update_params).and_call_original
+
+        result = ansible_cred.update_in_provider_queue(update_params)
+
+        expect(result).to be_truthy
+        expect(ansible_cred.reload.name).to eq("Updated Credential")
+      end
+    end
+
+    context "DELETE" do
+      let(:ansible_cred) { credential_class.raw_create_in_provider(nil, params.merge(:manager => manager)) }
+
+      it "#delete_in_provider will delete the record" do
+        expect(Notification).to receive(:create!).never
+
+        result = ansible_cred.delete_in_provider
+
+        expect(result).to be_truthy
+        expect { credential_class.find(ansible_cred.id) }.to raise_error ActiveRecord::RecordNotFound
+      end
+
+      it "#delete_in_provider_queue just calls raw_delete_in_provider" do
+        expect(ansible_cred).to receive(:delete_in_provider_queue).and_call_original
+
+        result = ansible_cred.delete_in_provider_queue
+
+        expect(result).to be_truthy
+        expect { credential_class.find(ansible_cred.id) }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
 end
